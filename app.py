420b2644f4afae8faa6f5dc3dfb5c5f3063a5e01
Serena Amro Gazze <samrogazze2022@fau.edu>
2026-04-25 23:34:27 -0400

    Final local

--- app.py
+++ app.py
@@ -140,8 +140,5 @@ def health():
 
 
 """ RUN APP """
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
